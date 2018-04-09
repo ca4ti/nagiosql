8fbbdbe38998ae6ace31f5b2049d0251e77a6cc0
wizonet <dgb.2018@wizonet.ch>
2018-04-09 17:30:40 +0200

    Updated password an user file Removed licence (changed from old BSD to new GPL v3)

--- admin/password.php
+++ admin/password.php
@@ -5,71 +5,83 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
-// (c) 2005-2017 by Martin Willisegger
+// (c) 2005-2018 by Martin Willisegger
 //
 // Project   : NagiosQL
 // Component : Password administration
-// Website   : http://www.nagiosql.org
-// Date      : $LastChangedDate: 2017-06-22 09:29:35 +0200 (Thu, 22 Jun 2017) $
-// Author    : $LastChangedBy: martin $
-// Version   : 3.3.0
-// Revision  : $LastChangedRevision: 2 $
+// Website   : https://sourceforge.net/projects/nagiosql/
+// Version   : 3.4.0
+// GIT Repo  : https://gitlab.com/wizonet/NagiosQL
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
+// Path settings
+// ===================
+$preRelPath  = strchr(filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING), 'admin', true);
+$preBasePath = filter_input(INPUT_SERVER, 'DOCUMENT_ROOT', FILTER_SANITIZE_STRING).$preRelPath;
+//
 // Define common variables
 // =======================
-$prePageId			= 31;
-$preContent   		= "admin/admin_master.tpl.htm";
-$preAccess    		= 1;
-$preFieldvars 		= 1;
-$preShowHeader  	= 0;
+$prePageId     = 31;
+$preContent    = "admin/admin_master.htm.tpl";
+$preAccess     = 1;
+$preFieldvars  = 1;
+$preShowHeader = 0;
 //
 // Include preprocessing files
 // ===========================
-require("../functions/prepend_adm.php");
-require("../functions/prepend_content.php");
+require($preBasePath.'functions/prepend_adm.php');
+require($preBasePath.'functions/prepend_content.php');
 //
 // Change password
 // =======================
 if (($chkTfValue1 != "") && ($chkTfValue2 != "")) {
-  	// Check old password
-  	$strSQL    = "SELECT * FROM `tbl_user` WHERE `username`='".$_SESSION['username']."' AND `password`=MD5('$chkTfValue1')";
-  	$booReturn = $myDBClass->getDataArray($strSQL,$arrDataLines,$intDataCount);
-  	if ($booReturn == false) {
-		$myVisClass->processMessage(translate('Error while selecting data from database:'),$strErrorMessage);
-		$myVisClass->processMessage($myDBClass->strErrorMessage,$strErrorMessage);
-  	} else if ($intDataCount == 1) {
-    	// Check equality and password length
-		if (($chkTfValue2 === $chkTfValue3) && (strlen($chkTfValue2) >=5)) {
-      		// Update database
-      		$strSQLUpdate = "UPDATE `tbl_user` SET `password`=MD5('$chkTfValue2'), 
-							 `last_login`=NOW() WHERE `username`='".$_SESSION['username']."'";
-      		$booReturn = $myDBClass->insertData($strSQLUpdate);
-      		if ($booReturn == true) {
-        		$myDataClass->writeLog(translate('Password successfully modified'));
-        		// Force new login
-        		$_SESSION['logged_in'] = 0;
-    			$_SESSION['username']  = "";
-				$_SESSION['userid']    = 0;
-				$_SESSION['groupadm']  = 0;
-				$_SESSION['domain']    = 0;
-        		header("Location: ".$SETS['path']['protocol']."://".$_SERVER['HTTP_HOST'].$_SESSION['SETS']['path']['base_url']."index.php");
-      		} else {
-				$myVisClass->processMessage(translate('Error while selecting data from database:'),$strErrorMessage);
-				$myVisClass->processMessage($myDBClass->strErrorMessage,$strErrorMessage);
-      		}
-    	} else {
-      		// New password wrong 
-			$myVisClass->processMessage(translate('Password too short or password fields unequally!'),$strErrorMessage);
-    	}
-  	} else {
-    	// Old password wrong
-		$myVisClass->processMessage(translate('Old password is wrong'),$strErrorMessage);
-  	}
-} else if (isset($_POST['submit'])) {
-  	// Wrong data
-	$myVisClass->processMessage(translate('Database entry failed! Not all necessary data filled in!'),$strErrorMessage);
+    // Check old password
+    $strSQL    = "SELECT * FROM `tbl_user` "
+        . "WHERE `username`='".$_SESSION['username']."' AND `password`=MD5('$chkTfValue1')";
+    $booReturn = $myDBClass->getDataArray($strSQL, $arrDataLines, $intDataCount);
+    if ($booReturn == false) {
+        $myVisClass->processMessage(translate('Error while selecting data from database:'), $strErrorMessage);
+        $myVisClass->processMessage($myDBClass->strErrorMessage, $strErrorMessage);
+    } elseif ($intDataCount == 1) {
+        // Check equality and password length
+        if (($chkTfValue2 === $chkTfValue3) && (strlen($chkTfValue2) >=5)) {
+            // Update database
+            $strSQLUpdate = "UPDATE `tbl_user` SET `password`=MD5('$chkTfValue2'), `last_login`=NOW() "
+                . "WHERE `username`='".$_SESSION['username']."'";
+            $booReturn = $myDBClass->insertData($strSQLUpdate);
+            if ($booReturn == true) {
+                $myDataClass->writeLog(translate('Password successfully modified'));
+                // Force new login
+                $_SESSION['logged_in'] = 0;
+                $_SESSION['username']  = "";
+                $_SESSION['userid']    = 0;
+                $_SESSION['groupadm']  = 0;
+                $_SESSION['domain']    = 0;
+                header("Location: ".$SETS['path']['protocol']."://".
+                    filter_input(INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING).
+                    $_SESSION['SETS']['path']['base_url']."index.php");
+            } else {
+                $myVisClass->processMessage(translate('Error while selecting data from database:'), $strErrorMessage);
+                $myVisClass->processMessage($myDBClass->strErrorMessage, $strErrorMessage);
+            }
+        } else {
+            // New password wrong
+            $myVisClass->processMessage(
+                translate('Password too short or password fields unequally!'),
+                $strErrorMessage
+            );
+        }
+    } else {
+        // Old password wrong
+        $myVisClass->processMessage(translate('Old password is wrong'), $strErrorMessage);
+    }
+} elseif (filter_input(INPUT_POST, 'submit')) {
+    // Wrong data
+    $myVisClass->processMessage(
+        translate('Database entry failed! Not all necessary data filled in!'),
+        $strErrorMessage
+    );
 }
 //
 // Output header variable
@@ -78,25 +90,29 @@ echo $tplHeaderVar;
 //
 // Include content
 // ===============
-foreach($arrDescription AS $elem) {
-  	$conttp->setVariable($elem['name'],$elem['string']);
+foreach ($arrDescription as $elem) {
+    $conttp->setVariable($elem['name'], $elem['string']);
 }
-$conttp->setVariable("LANG_SAVE",translate('Save'));
-$conttp->setVariable("LANG_ABORT",translate('Abort'));
-$conttp->setVariable("FILL_ALLFIELDS",translate('Please fill in all fields marked with an *'));
-$conttp->setVariable("FILL_NEW_PASSWD_NOT_EQUAL",translate('The new passwords are not equal!'));
-$conttp->setVariable("FILL_NEW_PWDSHORT",translate('The new password is too short - use at least 6 characters!'));
-if ($strErrorMessage != "") $conttp->setVariable("ERRORMESSAGE",$strErrorMessage);
-$conttp->setVariable("ACTION_INSERT",filter_var($_SERVER['PHP_SELF'], FILTER_SANITIZE_STRING));
-$conttp->setVariable("IMAGE_PATH",$_SESSION['SETS']['path']['base_url']."images/");
+$conttp->setVariable("LANG_SAVE", translate('Save'));
+$conttp->setVariable("LANG_ABORT", translate('Abort'));
+$conttp->setVariable("FILL_ALLFIELDS", translate('Please fill in all fields marked with an *'));
+$conttp->setVariable("FILL_NEW_PASSWD_NOT_EQUAL", translate('The new passwords are not equal!'));
+$conttp->setVariable("FILL_NEW_PWDSHORT", translate('The new password is too short - use at least 6 characters!'));
+if ($strErrorMessage != "") {
+    $conttp->setVariable("ERRORMESSAGE", $strErrorMessage);
+}
+$conttp->setVariable("ACTION_INSERT", filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING));
+$conttp->setVariable("IMAGE_PATH", $_SESSION['SETS']['path']['base_url']."images/");
 // Check access rights for adding new objects
-if ($myVisClass->checkAccGroup($prePageKey,'write') != 0) $conttp->setVariable("ADD_CONTROL","disabled=\"disabled\"");
+if ($myVisClass->checkAccountGroup($prePageKey, 'write') != 0) {
+    $conttp->setVariable("ADD_CONTROL", "disabled=\"disabled\"");
+}
 $conttp->parse("passwordsite");
 $conttp->show("passwordsite");
 //
 // Include footer
 // ==============
-$maintp->setVariable("VERSION_INFO","<a href='http://www.nagiosql.org' target='_blank'>NagiosQL</a> $setFileVersion");
+$maintp->setVariable("VERSION_INFO", "<a href='https://sourceforge.net/projects/nagiosql/' "
+    . "target='_blank'>NagiosQL</a> $setFileVersion");
 $maintp->parse("footer");
 $maintp->show("footer");
-?>
--- admin/user.php
+++ admin/user.php
@@ -5,171 +5,209 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
-// (c) 2005-2017 by Martin Willisegger
+// (c) 2005-2018 by Martin Willisegger
 //
 // Project   : NagiosQL
 // Component : User administration
-// Website   : http://www.nagiosql.org
-// Date      : $LastChangedDate: 2017-06-22 09:29:35 +0200 (Thu, 22 Jun 2017) $
-// Author    : $LastChangedBy: martin $
-// Version   : 3.3.0
-// Revision  : $LastChangedRevision: 2 $
+// Website   : https://sourceforge.net/projects/nagiosql/
+// Version   : 3.4.0
+// GIT Repo  : https://gitlab.com/wizonet/NagiosQL
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
+// Path settings
+// ===================
+$preRelPath  = strchr(filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING), 'admin', true);
+$preBasePath = filter_input(INPUT_SERVER, 'DOCUMENT_ROOT', FILTER_SANITIZE_STRING).$preRelPath;
+//
 // Define common variables
 // =======================
-$prePageId			= 32;
-$preContent   		= "admin/user.tpl.htm";
-$preSearchSession	= 'user';
-$preTableName		= 'tbl_user';
-$preKeyField		= 'username';
-$preAccess    		= 1;
-$preFieldvars 		= 1;
-//$preNoAccessGrp		= 1;
+$prePageId        = 32;
+$preContent       = "admin/user.htm.tpl";
+$preSearchSession = 'user';
+$preTableName     = 'tbl_user';
+$preKeyField      = 'username';
+$preAccess        = 1;
+$preFieldvars     = 1;
+$preNoAccessGrp   = 1;
 //
 // Include preprocessing files
 // ===========================
-require("../functions/prepend_adm.php");
-require("../functions/prepend_content.php");
-// 
+require($preBasePath.'functions/prepend_adm.php');
+require($preBasePath.'functions/prepend_content.php');
+//
 // Add or modify data
 // ==================
 if ((($chkModus == "insert") || ($chkModus == "modify")) && ($intGlobalWriteAccess == 0)) {
-  	// Check password
-  	if ((($chkTfValue3 === $chkTfValue4) && (strlen($chkTfValue3) > 5)) || (($chkModus == "modify") && ($chkTfValue3 == ""))) {
-    	if ($chkTfValue3 == "") {$strPasswd = "";} else {$strPasswd = "`password`=MD5('$chkTfValue3'),";}
-    	// Grant admin rights
-		if ($chkTfValue5 == "Admin") { 
-			$chkTfValue1  = "Admin";
-			$chkActive	  = "1";
-			$chkChbValue1 = "1";
-		}
-    	$strSQLx = "`$preTableName` SET `$preKeyField`='$chkTfValue1', `alias`='$chkTfValue2', $strPasswd `admin_enable`='$chkChbValue1', 
-				    `wsauth`='$chkChbValue2', `active`='$chkActive', `language`='$chkSelValue1', `domain`='$chkSelValue2', `last_modified`=NOW()";
-		if ($chkModus == "insert") {
-			$strSQL 		= "INSERT INTO ".$strSQLx;
-		} else {
-			$strSQL			= "UPDATE ".$strSQLx." WHERE `id`=$chkDataId";
-		}
-		if ($intWriteAccessId == 0) {
-			if (($chkTfValue1 != "") && ($chkTfValue2 != "")) {
-				$intReturn = $myDataClass->dataInsert($strSQL,$intInsertId);
-				if ($intReturn == 1) {
-					$myVisClass->processMessage($myDataClass->strErrorMessage,$strErrorMessage);
-				} else {
-					$myVisClass->processMessage($myDataClass->strInfoMessage,$strInfoMessage);
-					if ($chkModus == "insert") $myDataClass->writeLog(translate('A new user added:')." ".$chkTfValue1);
-					if ($chkModus == "modify") $myDataClass->writeLog(translate('User modified:')." ".$chkTfValue1);
-				}
-			} else {
-				$myVisClass->processMessage(translate('Database entry failed! Not all necessary data filled in!'),$strErrorMessage);
-			}
-		} else {
-			$myVisClass->processMessage(translate('Database entry failed! No write access!'),$strErrorMessage);
-		}
-  	} else {
-		$myVisClass->processMessage(translate('Password too short or password fields unequally!'),$strErrorMessage);
-  	}
-  	$chkModus = "display";
+    // Check password
+    if ((($chkTfValue3 === $chkTfValue4) && (strlen($chkTfValue3) > 5)) ||
+        (($chkModus == "modify") && ($chkTfValue3 == ""))) {
+        if ($chkTfValue3 == "") {
+            $strPasswd = "";
+        } else {
+            $strPasswd = "`password`=MD5('$chkTfValue3'),";
+        }
+        // Grant admin rights
+        if ($chkTfValue5 == "Admin") {
+            $chkTfValue1  = "Admin";
+            $chkActive      = "1";
+            $chkChbValue1 = "1";
+        }
+        $strSQLx = "`$preTableName` SET `$preKeyField`='$chkTfValue1', `alias`='$chkTfValue2', $strPasswd "
+            . "`admin_enable`='$chkChbValue1', `wsauth`='$chkChbValue2', `active`='$chkActive', "
+            . "`language`='$chkSelValue1', `domain`='$chkSelValue2', `last_modified`=NOW()";
+        if ($chkModus == "insert") {
+            $strSQL = "INSERT INTO ".$strSQLx;
+        } else {
+            $strSQL = "UPDATE ".$strSQLx." WHERE `id`=".$chkDataId;
+        }
+        if ($intWriteAccessId == 0) {
+            if (($chkTfValue1 != "") && ($chkTfValue2 != "")) {
+                $intReturn = $myDataClass->dataInsert($strSQL, $intInsertId);
+                if ($intReturn == 1) {
+                    $myVisClass->processMessage($myDataClass->strErrorMessage, $strErrorMessage);
+                } else {
+                    $myVisClass->processMessage($myDataClass->strInfoMessage, $strInfoMessage);
+                    if ($chkModus == "insert") {
+                        $myDataClass->writeLog(translate('A new user added:')." ".$chkTfValue1);
+                    }
+                    if ($chkModus == "modify") {
+                        $myDataClass->writeLog(translate('User modified:')." ".$chkTfValue1);
+                    }
+                }
+            } else {
+                $myVisClass->processMessage(
+                    translate('Database entry failed! Not all necessary data filled in!'),
+                    $strErrorMessage
+                );
+            }
+        } else {
+            $myVisClass->processMessage(translate('Database entry failed! No write access!'), $strErrorMessage);
+        }
+    } else {
+        $myVisClass->processMessage(translate('Password too short or password fields unequally!'), $strErrorMessage);
+    }
+    $chkModus = "display";
 }
-if ($chkModus != "add") $chkModus    = "display"; 
+if ($chkModus != "add") {
+    $chkModus = "display";
+}
 //
 // Start content
 // =============
-$conttp->setVariable("TITLE",translate('User administration'));
+$conttp->setVariable("TITLE", translate('User administration'));
 $conttp->parse("header");
 $conttp->show("header");
 //
 // Singe data form
 // ===============
 if ($chkModus == "add") {
-  	// Process domain selection field
-  	if (isset($arrModifyData['domain'])) {$intFieldId = $arrModifyData['domain'];} else {$intFieldId = 1;}
-  	$intReturn = $myVisClass->parseSelectSimple('tbl_datadomain','domain','std_domain',0,$intFieldId,0);
-	if  ($intReturn != 0) $myVisClass->processMessage($myVisClass->strErrorMessage,$strErrorMessage);
-  	// Process language selection field
-  	if (isset($arrModifyData['language'])) {$intFieldId = $arrModifyData['language'];} else {$intFieldId = 0;}
-	if ($intFieldId == 0) {
-		$intFieldId = $myDBClass->getFieldData("SELECT `id` FROM `tbl_language` WHERE `locale`='".$_SESSION['SETS']['data']['locale']."'")+0;
-	}
-  	$intReturn = $myVisClass->parseSelectSimple('tbl_language','language','language_name',0,$intFieldId);
-	if  ($intReturn != 0) $myVisClass->processMessage($myVisClass->strErrorMessage,$strErrorMessage);
-	// Initial add/modify form definitions
-	$myContentClass->addFormInit($conttp);
-	$conttp->setVariable("WSAUTH_DISABLE","disabled");
-	$conttp->setVariable("FILL_ALLFIELDS",translate('Please fill in all fields marked with an *'));
-	$conttp->setVariable("FILL_ILLEGALCHARS",translate('The following field contains not permitted characters:'));
-	$conttp->setVariable("FILL_PASSWD_NOT_EQUAL",translate('The passwords are not equal!'));
-	$conttp->setVariable("FILL_PASSWORD",translate('Please fill in the password'));
-	$conttp->setVariable("FILL_PWDSHORT",translate('The password is too short - use at least 6 characters!'));
-	$conttp->setVariable("LANG_WEBSERVER_AUTH",translate('Webserver authentification'));
-	$conttp->setVariable("PASSWORD_MUST","class=\"inpmust\"");
-	$conttp->setVariable("PASSWORD_MUST_STAR","*");
-  	// If webserver authetification is enabled - show option field
-  	if (isset($SETS['security']['wsauth']) && ($SETS['security']['wsauth'] == 1)) {
-    	$conttp->setVariable("WSAUTH_DISABLE","");
-  	}
-  	// Insert data from database in "modify" mode
-  	if (isset($arrModifyData) && ($chkSelModify == "modify")) {
-		// Process data
-		$myContentClass->addInsertData($conttp,$arrModifyData,0,'');
-		// Webserver authentification
-		$conttp->setVariable("WSAUTH_CHECKED","");
-		if ($arrModifyData['wsauth'] == 1) $conttp->setVariable("WSAUTH_CHECKED","checked");
-		// Object based group administration
-		$conttp->setVariable("ADMINENABLE_CHECKED","");
-		if ($arrModifyData['admin_enable'] == 1) $conttp->setVariable("ADMINENABLE_CHECKED","checked");
-		// Admin rules
-   		if ($arrModifyData[$preKeyField] == "Admin") {
-			$conttp->setVariable("NAME_DISABLE","disabled");
-			$conttp->setVariable("ACT_DISABLE","disabled");
-			$conttp->setVariable("WSAUTH_DISABLE","disabled");
-			$conttp->setVariable("ADMINENABLE_DISABLE","disabled");
-			$conttp->setVariable("ADMINENABLE_CHECKED","checked");
-    	}
-		$conttp->setVariable("PASSWORD_MUST","");
-		$conttp->setVariable("PASSWORD_MUST_STAR","");
-  	}
-  	$conttp->parse("datainsert");
-  	$conttp->show("datainsert");
+    // Process domain selection field
+    if (isset($arrModifyData['domain'])) {
+        $intFieldId = $arrModifyData['domain'];
+    } else {
+        $intFieldId = 1;
+    }
+    $intReturn1 = $myVisClass->parseSelectSimple('tbl_datadomain', 'domain', 'std_domain', 0, $intFieldId, 0);
+    if ($intReturn1 != 0) {
+        $myVisClass->processMessage($myVisClass->strErrorMessage, $strErrorMessage);
+    }
+    // Process language selection field
+    if (isset($arrModifyData['language'])) {
+        $intFieldId = $arrModifyData['language'];
+    } else {
+        $intFieldId = 0;
+    }
+    if ($intFieldId == 0) {
+        $intFieldId = $myDBClass->getFieldData("SELECT `id` FROM `tbl_language` "
+                . "WHERE `locale`='".$_SESSION['SETS']['data']['locale']."'")+0;
+    }
+    $intReturn2 = $myVisClass->parseSelectSimple('tbl_language', 'language', 'language_name', 0, $intFieldId);
+    if ($intReturn2 != 0) {
+        $myVisClass->processMessage($myVisClass->strErrorMessage, $strErrorMessage);
+    }
+    // Initial add/modify form definitions
+    $myContentClass->addFormInit($conttp);
+    $conttp->setVariable("WSAUTH_DISABLE", "disabled");
+    $conttp->setVariable("FILL_ALLFIELDS", translate('Please fill in all fields marked with an *'));
+    $conttp->setVariable("FILL_ILLEGALCHARS", translate('The following field contains not permitted characters:'));
+    $conttp->setVariable("FILL_PASSWD_NOT_EQUAL", translate('The passwords are not equal!'));
+    $conttp->setVariable("FILL_PASSWORD", translate('Please fill in the password'));
+    $conttp->setVariable("FILL_PWDSHORT", translate('The password is too short - use at least 6 characters!'));
+    $conttp->setVariable("LANG_WEBSERVER_AUTH", translate('Webserver authentification'));
+    $conttp->setVariable("PASSWORD_MUST", "class=\"inpmust\"");
+    $conttp->setVariable("PASSWORD_MUST_STAR", "*");
+    // If webserver authetification is enabled - show option field
+    if (isset($SETS['security']['wsauth']) && ($SETS['security']['wsauth'] == 1)) {
+        $conttp->setVariable("WSAUTH_DISABLE", "");
+    }
+    // Insert data from database in "modify" mode
+    if (isset($arrModifyData) && ($chkSelModify == "modify")) {
+        // Process data
+        $myContentClass->addInsertData($conttp, $arrModifyData, 0, '');
+        // Webserver authentification
+        $conttp->setVariable("WSAUTH_CHECKED", "");
+        if ($arrModifyData['wsauth'] == 1) {
+            $conttp->setVariable("WSAUTH_CHECKED", "checked");
+        }
+        // Object based group administration
+        $conttp->setVariable("ADMINENABLE_CHECKED", "");
+        if ($arrModifyData['admin_enable'] == 1) {
+            $conttp->setVariable("ADMINENABLE_CHECKED", "checked");
+        }
+        // Admin rules
+        if ($arrModifyData[$preKeyField] == "Admin") {
+            $conttp->setVariable("NAME_DISABLE", "disabled");
+            $conttp->setVariable("ACT_DISABLE", "disabled");
+            $conttp->setVariable("WSAUTH_DISABLE", "disabled");
+            $conttp->setVariable("ADMINENABLE_DISABLE", "disabled");
+            $conttp->setVariable("ADMINENABLE_CHECKED", "checked");
+        }
+        $conttp->setVariable("PASSWORD_MUST", "");
+        $conttp->setVariable("PASSWORD_MUST_STAR", "");
+    }
+    $conttp->parse("datainsert");
+    $conttp->show("datainsert");
 }
 //
 // Data table
 // ==========
 if ($chkModus == "display") {
-	// Initial list view definitions
-	$myContentClass->listViewInit($mastertp);
-	$mastertp->setVariable("FIELD_1",translate('Username'));
-	$mastertp->setVariable("FIELD_2",translate('Description'));
-	// Row sorting
-	$strOrderString = "ORDER BY `$preKeyField` $hidSortDir";
-	if ($hidSortBy == 2) $strOrderString = "ORDER BY `alias` $hidSortDir";
-  	// Count datasets
-  	$strSQL    = "SELECT count(*) AS `number` FROM `$preTableName`";
-  	$booReturn = $myDBClass->getSingleDataset($strSQL,$arrDataLinesCount);
-  	if ($booReturn == false) {
-    	$myVisClass->processMessage(translate('Error while selecting data from database:'),$strErrorMessage);
-		$myVisClass->processMessage($myDBClass->strErrorMessage,$strErrorMessage);
-  	} else {
-    	$intLineCount = (int)$arrDataLinesCount['number'];
-		if ($intLineCount < $chkLimit) $chkLimit = 0;
-  	}
-  	// Get datasets
-  	$strSQL    = "SELECT `id`, `$preKeyField`, `alias`, `active`, `nodelete`
-          		  FROM `$preTableName` $strOrderString LIMIT $chkLimit,".$SETS['common']['pagelines'];
-  	$booReturn = $myDBClass->getDataArray($strSQL,$arrDataLines,$intDataCount);
-	if ($booReturn == false) {
-    	$myVisClass->processMessage(translate('Error while selecting data from database:'),$strErrorMessage);
-		$myVisClass->processMessage($myDBClass->strErrorMessage,$strErrorMessage);
-  	}
-	// Process data
-	$myContentClass->listData($mastertp,$arrDataLines,$intDataCount,$intLineCount,$preKeyField,'alias');
+    // Initial list view definitions
+    $myContentClass->listViewInit($mastertp);
+    $mastertp->setVariable("FIELD_1", translate('Username'));
+    $mastertp->setVariable("FIELD_2", translate('Description'));
+    // Row sorting
+    $strOrderString = "ORDER BY `$preKeyField` $hidSortDir";
+    if ($hidSortBy == 2) {
+        $strOrderString = "ORDER BY `alias` $hidSortDir";
+    }
+    // Count datasets
+    $strSQL     = "SELECT count(*) AS `number` FROM `$preTableName`";
+    $booReturn1 = $myDBClass->getSingleDataset($strSQL, $arrDataLinesCount);
+    if ($booReturn1 == false) {
+        $myVisClass->processMessage(translate('Error while selecting data from database:'), $strErrorMessage);
+        $myVisClass->processMessage($myDBClass->strErrorMessage, $strErrorMessage);
+    } else {
+        $intLineCount = (int)$arrDataLinesCount['number'];
+        if ($intLineCount < $chkLimit) {
+            $chkLimit = 0;
+        }
+    }
+    // Get datasets
+    $strSQL     = "SELECT `id`, `$preKeyField`, `alias`, `active`, `nodelete` "
+        . "FROM `$preTableName` $strOrderString LIMIT $chkLimit,".$SETS['common']['pagelines'];
+    $booReturn2 = $myDBClass->getDataArray($strSQL, $arrDataLines, $intDataCount);
+    if ($booReturn2 == false) {
+        $myVisClass->processMessage(translate('Error while selecting data from database:'), $strErrorMessage);
+        $myVisClass->processMessage($myDBClass->strErrorMessage, $strErrorMessage);
+    }
+    // Process data
+    $myContentClass->listData($mastertp, $arrDataLines, $intDataCount, $intLineCount, $preKeyField, 'alias');
 }
 // Show messages
-$myContentClass->showMessages($mastertp,$strErrorMessage,$strInfoMessage,$strConsistMessage,'','',1);
+$myContentClass->showMessages($mastertp, $strErrorMessage, $strInfoMessage, $strConsistMessage, array(), '', 1);
 //
 // Process footer
 // ==============
-$myContentClass->showFooter($maintp,$setFileVersion);
-?>
+$myContentClass->showFooter($maintp, $setFileVersion);
