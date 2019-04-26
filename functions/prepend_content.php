788e24b17932e218ec897e572073eb9afe828723
wizonet <dgb.2018@wizonet.ch>
2019-04-26 18:07:17 +0200

    Allowing special characters in ARG fields

--- functions/prepend_content.php
+++ functions/prepend_content.php
@@ -72,7 +72,7 @@ for ($i = 1; $i <= 22; $i++) {
 // Common argument text field value
 for ($i = 1; $i <= 8; $i++) {
     $tmpVar  = 'chkTfArg'.$i;
-    $$tmpVar = filter_input(INPUT_POST, 'tfArg'.$i, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $$tmpVar = filter_input(INPUT_POST, 'tfArg'.$i, FILTER_UNSAFE_RAW);
     if (get_magic_quotes_gpc() == 0) {
         $$tmpVar = addslashes($$tmpVar);
     }
@@ -80,7 +80,6 @@ for ($i = 1; $i <= 8; $i++) {
         $$tmpVar = $myVisClass->tfSecure($$tmpVar);
     }
 }
-
 // Common multi select field value
 for ($i = 1; $i <= 8; $i++) {
     $tmpVar  = 'chkMselValue'.$i;
