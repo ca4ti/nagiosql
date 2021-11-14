ec51b44c02eaafa7bd53621237382e4f0af8f6b6
wzowim00 <dgb.2018@wizonet.ch>
2021-11-14 16:53:59 +0100

    Release warning

--- index.php
+++ index.php
@@ -1,49 +1,53 @@
 <?php
-///////////////////////////////////////////////////////////////////////////////
-//
-// NagiosQL
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-// (c) 2005-2022 by Martin Willisegger
-//
-// Project   : NagiosQL
-// Component : Start script
-// Website   : https://sourceforge.net/projects/nagiosql/
-// Version   : 3.5.0
-// GIT Repo  : https://gitlab.com/wizonet/NagiosQL
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-// Path settings
-// ===================
-$preRelPath  = strstr(filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING), 'index.php', true);
+/* ----------------------------------------------------------------------------
+ NagiosQL
+-------------------------------------------------------------------------------
+ (c) 2005-2022 by Martin Willisegger
+
+ Project   : NagiosQL
+ Component : Start script
+ Website   : https://sourceforge.net/projects/nagiosql/
+ Version   : 3.5.0
+ GIT Repo  : https://gitlab.com/wizonet/NagiosQL
+-----------------------------------------------------------------------------*/
+/**
+ * Class and variable includes
+ * @var HTML_Template_IT $conttp
+ * @var HTML_Template_IT $maintp
+ * @var string $setFileVersion from prepend_adm.php
+ */
+echo "Nagiosql 3.5.0 is not yet released - the version basically does not work correctly and must not be used for productive use. Please download version 3.4.1!";
+exit;
+/*
+Path settings
+*/
+$preRelPath = strstr(filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING), 'index.php', true);
 $preBasePath = strstr(filter_input(INPUT_SERVER, 'SCRIPT_FILENAME', FILTER_SANITIZE_STRING), 'index.php', true);
-//
-// Destroy old session data
-// ========================
+/*
+Destroy old session data
+*/
 session_start();
 session_destroy();
-//
-// Define common variables
-// =======================
-$intPageID  = 0;
+/*
+Define common variables
+*/
+$intPageID = 0;
 $preContent = 'index.htm.tpl';
-//
-// Redirect to installation wizard
-// ===============================
+/*
+Redirect to installation wizard
+*/
 if (PHP_VERSION_ID < 50300) {
     header('Location: install/index.php');
 }
-//
-// Include preprocessing file
-// ==========================
-$preAccess    = 0;
+/*
+Include preprocessing file
+*/
+$preAccess = 0;
 $preFieldvars = 0;
-require $preBasePath.'functions/prepend_adm.php';
-//
-// Include Content
-// ===============
+require 'functions/prepend_adm.php';
+/*
+Include Content
+*/
 $conttp->setVariable('TITLE', translate('Welcome to'));
 $conttp->setVariable('TITLE_LOGIN', translate('Welcome'));
 $conttp->setVariable('LOGIN_TEXT', translate('Please enter your username and password to access NagiosQL.<br>If '
@@ -51,7 +55,7 @@ $conttp->setVariable('LOGIN_TEXT', translate('Please enter your username and pas
 $conttp->setVariable('USERNAME', translate('Username'));
 $conttp->setVariable('PASSWORD', translate('Password'));
 $conttp->setVariable('LOGIN', translate('Login'));
-if (isset($_SESSION['strLoginMessage']) && ($_SESSION['strLoginMessage'] != '')) {
+if (isset($_SESSION['strLoginMessage']) && ($_SESSION['strLoginMessage'] !== '')) {
     $conttp->setVariable('MESSAGE', $_SESSION['strLoginMessage']);
 } else {
     $conttp->setVariable('MESSAGE', '&nbsp;');
@@ -60,9 +64,9 @@ $conttp->setVariable('ACTION_INSERT', filter_input(INPUT_SERVER, 'PHP_SELF', FIL
 $conttp->setVariable('IMAGE_PATH', 'images/');
 $conttp->parse('main');
 $conttp->show('main');
-//
-// Include footer
-// ==============
+/*
+Include footer
+*/
 $maintp->setVariable('VERSION_INFO', "<a href='https://sourceforge.net/projects/nagiosql/' "
     . "target='_blank'>NagiosQL</a> $setFileVersion");
 $maintp->parse('footer');
