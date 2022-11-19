f300f39d7fb36613d203e9e7f120236ea6c4e718
wzowim00 <dgb.2018@wizonet.ch>
2022-11-19 23:02:36 +0100

    Finalize administration.php

--- admin/administration.php
+++ admin/administration.php
@@ -1,46 +1,48 @@
 <?php
-///////////////////////////////////////////////////////////////////////////////
-//
-// NagiosQL
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-// (c) 2005-2020 by Martin Willisegger
-//
-// Project   : NagiosQL
-// Component : Administration overview
-// Website   : https://sourceforge.net/projects/nagiosql/
-// Version   : 3.4.1
-// GIT Repo  : https://gitlab.com/wizonet/NagiosQL
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-// Path settings
-// ===================
+/* ----------------------------------------------------------------------------
+ NagiosQL
+-------------------------------------------------------------------------------
+ (c) 2005-2022 by Martin Willisegger
+
+ Project   : NagiosQL
+ Component : Administration overview
+ Website   : https://sourceforge.net/projects/nagiosql/
+ Version   : 3.5.0
+ GIT Repo  : https://gitlab.com/wizonet/NagiosQL
+-----------------------------------------------------------------------------*/
+/**
+ * Class and variable includes
+ * @var HTML_Template_IT $conttp
+ * @var HTML_Template_IT $maintp
+ * @var string $setFileVersion from prepend_adm.php
+ */
+/*
+Path settings
+*/
 $strPattern = '(admin/[^/]*.php)';
-$preRelPath  = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING));
-$preBasePath = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'SCRIPT_FILENAME', FILTER_SANITIZE_STRING));
-//
-// Define common variables
-// =======================
+$preRelPath  = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'PHP_SELF'));
+$preBasePath = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'SCRIPT_FILENAME'));
+/*
+Define common variables
+*/
 $prePageId  = 7;
 $preContent = 'admin/mainpages.htm.tpl';
-//
-// Include preprocessing file
-// ==========================
+/*
+Include preprocessing file
+*/
 require $preBasePath. 'functions/prepend_adm.php';
-//
-// Include content
-// ===============
+/*
+Include content
+*/
 $conttp->setVariable('TITLE', translate('Administration'));
 $conttp->parse('header');
 $conttp->show('header');
 $conttp->setVariable('DESC', translate('Functions to administrate NagiosQL V3'));
 $conttp->parse('main');
 $conttp->show('main');
-//
-// Include Footer
-// ==============
+/*
+Include Footer
+*/
 $maintp->setVariable('VERSION_INFO', "<a href='https://sourceforge.net/projects/nagiosql/' "
         . "target='_blank'>NagiosQL</a> $setFileVersion");
 $maintp->parse('footer');
