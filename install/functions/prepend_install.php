d2541f52d8083c748b1720875cc80f1d657c88c9
wzowim00 <dgb.2018@wizonet.ch>
2022-11-19 09:39:58 +0100

    Finalize prepend_install.php

--- install/functions/prepend_install.php
+++ install/functions/prepend_install.php
@@ -1,34 +1,35 @@
 <?php
-///////////////////////////////////////////////////////////////////////////////
-//
-// NagiosQL
-///////////////////////////////////////////////////////////////////////////////
-//
-// (c) 2005-2020 by Martin Willisegger
-//
-// Project   : NagiosQL
-// Component : Installer preprocessing script
-// Website   : https://sourceforge.net/projects/nagiosql/
-// Version   : 3.4.1
-// GIT Repo  : https://gitlab.com/wizonet/NagiosQL
-//
-///////////////////////////////////////////////////////////////////////////////
+/* ----------------------------------------------------------------------------
+ NagiosQL
+-------------------------------------------------------------------------------
+ (c) 2005-2022 by Martin Willisegger
+
+ Project   : NagiosQL
+ Component : Installer preprocessing script
+ Website   : https://sourceforge.net/projects/nagiosql/
+ Version   : 3.5.0
+ GIT Repo  : https://gitlab.com/wizonet/NagiosQL
+-----------------------------------------------------------------------------*/
 error_reporting(E_ALL);
-//
-// Define common variables
-// =======================
-$strErrorMessage = '';  // All error messages (red)
-$strInfoMessage  = '';  // All information messages (green)
-//
+/**
+ * Class and variable includes
+ * @var string $preBasePath from index.php
+ */
+/*
+Define common variables
+*/
+$strErrorMessage = '';  /* All error messages (red) */
+$strInfoMessage  = '';  /* All information messages (green) */
+/*
 // Start PHP session
-// =================
+*/
 session_start([ 'name' => 'nagiosql_install']);
-//
-// Include external function/class files
-// =====================================
+/*
+Include external function/class files
+*/
 require $preBasePath.'functions/Autoloader.php';
 functions\Autoloader::register($preBasePath);
-//
-// Initialize class
-// ================
+/*
+Initialize class
+*/
 $myInstClass = new install\functions\NagInstallClass($_SESSION);
