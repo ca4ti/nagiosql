a2dd55463ed09bdd018b56162478a1c1568f0768
wzowim00 <dgb.2018@wizonet.ch>
2022-11-20 22:13:17 +0100

    Finalize contactgroups.php

--- admin/contactgroups.php
+++ admin/contactgroups.php
@@ -1,95 +1,138 @@
 <?php
-///////////////////////////////////////////////////////////////////////////////
-//
-// NagiosQL
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-// (c) 2005-2020 by Martin Willisegger
-//
-// Project   : NagiosQL
-// Component : Contactgroup definition
-// Website   : https://sourceforge.net/projects/nagiosql/
-// Version   : 3.4.1
-// GIT Repo  : https://gitlab.com/wizonet/NagiosQL
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-// Path settings
-// ===================
+/* ----------------------------------------------------------------------------
+ NagiosQL
+-------------------------------------------------------------------------------
+ (c) 2005-2022 by Martin Willisegger
+
+ Project   : NagiosQL
+ Component : Contactgroup definition
+ Website   : https://sourceforge.net/projects/nagiosql/
+ Version   : 3.5.0
+ GIT Repo  : https://gitlab.com/wizonet/NagiosQL
+-----------------------------------------------------------------------------*/
+use functions\MysqliDbClass;
+use functions\NagConfigClass;
+use functions\NagContentClass;
+use functions\NagDataClass;
+use functions\NagVisualClass;
+
+/**
+ * Class and variable includes
+ * @var HTML_Template_IT $conttp
+ * @var HTML_Template_IT $maintp
+ * @var HTML_Template_IT $mastertp
+ * @var NagVisualClass $myVisClass
+ * @var MysqliDbClass $myDBClass
+ * @var NagDataClass $myDataClass
+ * @var NagConfigClass $myConfigClass
+ * @var NagContentClass $myContentClass
+ * @var string $setFileVersion from prepend_adm.php
+ * @var string $chkModus from prepend_adm.php
+ * @var int $chkDataId from prepend_adm.php
+ * @var int $chkListId from prepend_adm.php
+ * @var int $intVersion from prepend_adm.php
+ * @var string $chkSelModify from prepend_adm.php
+ * @var string $hidSortDir from prepend_adm.php
+ * @var int $hidSortBy from prepend_adm.php
+ * @var string $strDomainWhere from prepend_adm.php
+ * @var int $chkLimit from prepend_adm.php
+ * @var array $SETS from prepend_adm.php
+ * @var string $strConsistMessage from prepend_adm.php
+ * @var int $intGlobalWriteAccess from prepend_content.php
+ * @var int $intWriteAccessId from prepend_content.php
+ * @var string $preSQLCommon1 from prepend_content.php
+ * @var string $strAccess from prepend_content.php
+ * @var string $chkTfValue1 from prepend_content.php
+ * @var string $chkTfValue2 from prepend_content.php
+ * @var int $intMselValue1 from prepend_content.php
+ * @var int $intMselValue2 from prepend_content.php
+ * @var array $chkMselValue1 from prepend_content.php
+ * @var array $chkMselValue2 from prepend_content.php
+ *
+ *
+ */
+/*
+Path settings
+*/
 $strPattern = '(admin/[^/]*.php)';
-$preRelPath  = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING));
-$preBasePath = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'SCRIPT_FILENAME', FILTER_SANITIZE_STRING));
-//
-// Define common variables
-// =======================
-$prePageId        = 15;
-$preContent       = 'admin/contactgroups.htm.tpl';
-$preListTpl       = 'admin/datalist.htm.tpl';
+$preRelPath = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'PHP_SELF'));
+$preBasePath = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'SCRIPT_FILENAME'));
+/*
+Define common variables
+*/
+$prePageId = 15;
+$preContent = 'admin/contactgroups.htm.tpl';
+$preListTpl = 'admin/datalist.htm.tpl';
 $preSearchSession = 'contactgroup';
-$preTableName     = 'tbl_contactgroup';
-$preKeyField      = 'contactgroup_name';
-$preAccess        = 1;
-$preFieldvars     = 1;
-//
-// Include preprocessing files
-// ===========================
-require $preBasePath.'functions/prepend_adm.php';
-require $preBasePath.'functions/prepend_content.php';
-//
-// Add or modify data
-// ==================
-if ((($chkModus == 'insert') || ($chkModus == 'modify')) && ($intGlobalWriteAccess == 0)) {
+$preTableName = 'tbl_contactgroup';
+$preKeyField = 'contactgroup_name';
+$preAccess = 1;
+$preFieldvars = 1;
+$strErrorMessage = '';
+$strInfoMessage = '';
+$strDBWarning = '';
+$intRet1 = 0;
+$intRet2 = 0;
+$intNoTime = 0;
+/*
+Include preprocessing file
+*/
+require $preBasePath . 'functions/prepend_adm.php';
+require $preBasePath . 'functions/prepend_content.php';
+/*
+Add or modify data
+*/
+if ((($chkModus === 'insert') || ($chkModus === 'modify')) && ($intGlobalWriteAccess === 0)) {
     $strSQLx = "`$preTableName` SET `$preKeyField`='$chkTfValue1', `alias`='$chkTfValue2', `members`=$intMselValue1, "
-             . "`contactgroup_members`=$intMselValue2, $preSQLCommon1";
-    if ($chkModus == 'insert') {
-        $strSQL = 'INSERT INTO ' .$strSQLx;
+        . "`contactgroup_members`=$intMselValue2, $preSQLCommon1";
+    if ($chkModus === 'insert') {
+        $strSQL = 'INSERT INTO ' . $strSQLx;
     } else {
-        $strSQL = 'UPDATE ' .$strSQLx. ' WHERE `id`=' .$chkDataId;
+        $strSQL = 'UPDATE ' . $strSQLx . ' WHERE `id`=' . $chkDataId;
     }
-    if ($intWriteAccessId == 0) {
-        if (($chkTfValue1 != '') && ($chkTfValue2 != '') && ($intMselValue1 != 0)) {
+    if ($intWriteAccessId === 0) {
+        if (($chkTfValue1 !== '') && ($chkTfValue2 !== '') && ($intMselValue1 !== 0)) {
             $intReturn = $myDataClass->dataInsert($strSQL, $intInsertId);
-            if ($chkModus == 'insert') {
+            if ($chkModus === 'insert') {
                 $chkDataId = $intInsertId;
             }
-            if ($intReturn == 1) {
+            if ($intReturn === 1) {
                 $myVisClass->processMessage($myDataClass->strErrorMessage, $strErrorMessage);
             } else {
                 $myVisClass->processMessage($myDataClass->strInfoMessage, $strInfoMessage);
                 $myDataClass->updateStatusTable($preTableName);
-                if ($chkModus  == 'insert') {
+                if ($chkModus  === 'insert') {
                     $myDataClass->writeLog(translate('New contact group inserted:'). ' ' .$chkTfValue1);
                 }
-                if ($chkModus  == 'modify') {
+                if ($chkModus  === 'modify') {
                     $myDataClass->writeLog(translate('Contact group modified:'). ' ' .$chkTfValue1);
                 }
-                //
-                // Insert/update relations
-                // =======================
-                if ($chkModus == 'insert') {
-                    if ($intMselValue1 != 0) {
+                /*
+                Insert/update relations
+                */
+                if ($chkModus === 'insert') {
+                    if ($intMselValue1 !== 0) {
                         $intRet1 = $myDataClass->dataInsertRelation(
                             'tbl_lnkContactgroupToContact',
                             $chkDataId,
                             $chkMselValue1
                         );
                     }
-                    if (isset($intRet1) && ($intRet1 != 0)) {
+                    if (isset($intRet1) && ($intRet1 !== 0)) {
                         $myVisClass->processMessage($myDataClass->strErrorMessage, $strErrorMessage);
                     }
-                    if ($intMselValue2  != 0) {
+                    if ($intMselValue2  !== 0) {
                         $intRet2 = $myDataClass->dataInsertRelation(
                             'tbl_lnkContactgroupToContactgroup',
                             $chkDataId,
                             $chkMselValue2
                         );
                     }
-                    if (isset($intRet2) && ($intRet2 != 0)) {
+                    if (isset($intRet2) && ($intRet2 !== 0)) {
                         $myVisClass->processMessage($myDataClass->strErrorMessage, $strErrorMessage);
                     }
-                } elseif ($chkModus == 'modify') {
-                    if ($intMselValue1 != 0) {
+                } elseif ($chkModus === 'modify') {
+                    if ($intMselValue1 !== 0) {
                         $intRet1 = $myDataClass->dataUpdateRelation(
                             'tbl_lnkContactgroupToContact',
                             $chkDataId,
@@ -98,10 +141,10 @@ if ((($chkModus == 'insert') || ($chkModus == 'modify')) && ($intGlobalWriteAcce
                     } else {
                         $intRet1 = $myDataClass->dataDeleteRelation('tbl_lnkContactgroupToContact', $chkDataId);
                     }
-                    if ($intRet1 != 0) {
+                    if ($intRet1 !== 0) {
                         $myVisClass->processMessage($myDataClass->strErrorMessage, $strErrorMessage);
                     }
-                    if ($intMselValue2 != 0) {
+                    if ($intMselValue2 !== 0) {
                         $intRet2 = $myDataClass->dataUpdateRelation(
                             'tbl_lnkContactgroupToContactgroup',
                             $chkDataId,
@@ -110,11 +153,11 @@ if ((($chkModus == 'insert') || ($chkModus == 'modify')) && ($intGlobalWriteAcce
                     } else {
                         $intRet2 = $myDataClass->dataDeleteRelation('tbl_lnkContactgroupToContactgroup', $chkDataId);
                     }
-                    if ($intRet2 != 0) {
+                    if ($intRet2 !== 0) {
                         $myVisClass->processMessage($myDataClass->strErrorMessage, $strErrorMessage);
                     }
                 }
-                if (($intRet1 + $intRet2) != 0) {
+                if (($intRet1 + $intRet2) !== 0) {
                     $strInfoMessage = '';
                 }
             }
@@ -129,29 +172,26 @@ if ((($chkModus == 'insert') || ($chkModus == 'modify')) && ($intGlobalWriteAcce
     }
     $chkModus = 'display';
 }
-if ($chkModus != 'add') {
+if ($chkModus !== 'add') {
     $chkModus    = 'display';
 }
-//
-// Get date/time of last database and config file manipulation
-// ===========================================================
+/*
+Get date/time of last database and config file manipulation
+*/
 $intReturn = $myConfigClass->lastModifiedFile($preTableName, $arrTimeData, $strTimeInfoString);
-if ($intReturn != 0) {
+if ($intReturn !== 0) {
     $myVisClass->processMessage($myConfigClass->strErrorMessage, $strErrorMessage);
 }
-//
-// Singe data form
-// ===============
-if ($chkModus == 'add') {
+/*
+Singe data form
+*/
+if ($chkModus === 'add') {
+    $intDataWarning = 0;
     $conttp->setVariable('TITLE', translate('Define contact groups (contactgroups.cfg)'));
-    // Do not show modified time list
+    /* Do not show modified time list */
     $intNoTime = 1;
-    // Process contact member selection fields
-    if (isset($arrModifyData['members'])) {
-        $intFieldId = $arrModifyData['members'];
-    } else {
-        $intFieldId = 0;
-    }
+    /* Process access group selection field */
+    $intFieldId = $arrModifyData['members'] ?? 0;
     $intReturn1 = $myVisClass->parseSelectMulti(
         'tbl_contact',
         'contact_name',
@@ -160,17 +200,13 @@ if ($chkModus == 'add') {
         2,
         $intFieldId
     );
-    if ($intReturn1 != 0) {
+    if ($intReturn1 !== 0) {
         $myVisClass->processMessage($myVisClass->strErrorMessage, $strErrorMessage);
         $myVisClass->processMessage(translate('Attention, no contacts defined!'), $strDBWarning);
         $intDataWarning = 1;
     }
-    // Process contactgroup member selection fields
-    if (isset($arrModifyData['contactgroup_members'])) {
-        $intFieldId = $arrModifyData['contactgroup_members'];
-    } else {
-        $intFieldId = 0;
-    }
+    /* Process contactgroup member selection fields */
+    $intFieldId = $arrModifyData['contactgroup_members'] ?? 0;
     $intReturn2 = $myVisClass->parseSelectMulti(
         $preTableName,
         $preKeyField,
@@ -180,66 +216,65 @@ if ($chkModus == 'add') {
         $intFieldId,
         $chkListId
     );
-    if ($intReturn2 != 0) {
+    if ($intReturn2 !== 0) {
         $myVisClass->processMessage($myVisClass->strErrorMessage, $strErrorMessage);
     }
-    // Process acces group selection field
-    if (isset($arrModifyData['access_group'])) {
-        $intFieldId = $arrModifyData['access_group'];
-    } else {
-        $intFieldId = 0;
-    }
+    /* Process acces group selection field */
+    $intFieldId = $arrModifyData['access_group'] ?? 0;
     $intReturn3 = $myVisClass->parseSelectSimple('tbl_group', 'groupname', 'acc_group', 0, $intFieldId);
-    if ($intReturn3 != 0) {
+    if ($intReturn3 !== 0) {
         $myVisClass->processMessage($myVisClass->strErrorMessage, $strErrorMessage);
     }
-    // Initial add/modify form definitions
+    /* Initial add/modify form definitions */
     $myContentClass->addFormInit($conttp);
-    if ($intDataWarning == 1) {
+    if ($intDataWarning === 1) {
         $conttp->setVariable('WARNING', $strDBWarning. '<br>' .translate('Saving not possible!'));
     }
     if ($intVersion < 3) {
         $conttp->setVariable('VERSION_20_VALUE_MUST', 'mselValue1,');
     }
-    // Insert data from database in "modify" mode
-    if (isset($arrModifyData) && ($chkSelModify == 'modify')) {
-        // Check relation information to find out locked configuration datasets
+    /* Insert data from database in "modify" mode */
+    if (isset($arrModifyData) && ($chkSelModify === 'modify')) {
+        /* Check relation information to find out locked configuration datasets */
         $intLocked = $myDataClass->infoRelation($preTableName, $arrModifyData['id'], $preKeyField);
         $myVisClass->processMessage($myDataClass->strInfoMessage, $strRelMessage);
         $strInfo  = '<br><span class="redmessage">' .translate('Entry cannot be activated because it is used by '
                   . 'another configuration'). ':</span>';
         $strInfo .= '<br><span class="greenmessage">' .$strRelMessage. '</span>';
-        // Process data
+        /* Process data */
         $myContentClass->addInsertData($conttp, $arrModifyData, $intLocked, $strInfo);
     }
     $conttp->parse('datainsert');
     $conttp->show('datainsert');
 }
-//
-// List view
-// ==========
-if ($chkModus == 'display') {
-    // Initial list view definitions
+/*
+List view
+*/
+if ($chkModus === 'display') {
+    /* Initial list view definitions */
+    $strSearchWhere = '';
     $myContentClass->listViewInit($mastertp);
     $mastertp->setVariable('TITLE', translate('Define contact groups (contactgroups.cfg)'));
     $mastertp->setVariable('FIELD_1', translate('Contact group'));
     $mastertp->setVariable('FIELD_2', translate('Description'));
     $mastertp->setVariable('FILTER_VISIBLE', 'visibility: hidden');
-    // Process filter string
-    if ($_SESSION['search'][$preSearchSession] != '') {
+    /* Process filter string */
+    if ($_SESSION['search'][$preSearchSession] !== '') {
         $strSearchTxt   = $_SESSION['search'][$preSearchSession];
         $strSearchWhere = "AND (`$preKeyField` LIKE '%".$strSearchTxt."%' OR `alias` LIKE '%".$strSearchTxt."%')";
     }
-    // Row sorting
+    /* Row sorting */
     $strOrderString = "ORDER BY `config_id`, `$preKeyField` $hidSortDir";
-    if ($hidSortBy == 2) {
+    if ($hidSortBy === 2) {
         $strOrderString = "ORDER BY `config_id`, `alias` $hidSortDir";
     }
-    // Count datasets
+    /* Count datasets */
+    $intLineCount = 0;
+    /** @noinspection SqlResolve */
     $strSQL     = "SELECT count(*) AS `number` FROM `$preTableName` "
                 . "WHERE $strDomainWhere $strSearchWhere AND `access_group` IN ($strAccess)";
     $booReturn1 = $myDBClass->hasSingleDataset($strSQL, $arrDataLinesCount);
-    if ($booReturn1 == false) {
+    if ($booReturn1 === false) {
         $myVisClass->processMessage(translate('Error while selecting data from database:'), $strErrorMessage);
         $myVisClass->processMessage($myDBClass->strErrorMessage, $strErrorMessage);
     } else {
@@ -248,19 +283,19 @@ if ($chkModus == 'display') {
             $chkLimit = 0;
         }
     }
-    // Get datasets
+    /* Get datasets */
     $strSQL     = "SELECT `id`, `$preKeyField`, `alias`, `register`, `active`, `config_id`, `access_group` "
                 . "FROM `$preTableName` WHERE $strDomainWhere $strSearchWhere AND `access_group` IN ($strAccess) "
                 . "$strOrderString LIMIT $chkLimit,".$SETS['common']['pagelines'];
     $booReturn2 = $myDBClass->hasDataArray($strSQL, $arrDataLines, $intDataCount);
-    if ($booReturn2 == false) {
+    if ($booReturn2 === false) {
         $myVisClass->processMessage(translate('Error while selecting data from database:'), $strErrorMessage);
         $myVisClass->processMessage($myDBClass->strErrorMessage, $strErrorMessage);
     }
-    // Process data
+    /* Process data */
     $myContentClass->listData($mastertp, $arrDataLines, $intDataCount, $intLineCount, $preKeyField, 'alias');
 }
-// Show messages
+/* Show messages */
 $myContentClass->showMessages(
     $mastertp,
     $strErrorMessage,
@@ -270,7 +305,7 @@ $myContentClass->showMessages(
     $strTimeInfoString,
     $intNoTime
 );
-//
-// Process footer
-// ==============
+/*
+Process footer
+ */
 $myContentClass->showFooter($maintp, $setFileVersion);
